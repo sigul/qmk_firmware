6a4eadffa94b9606a7f42c900a87d2dd2a116933
sigul <desk@silviogulizia.com>
2020-10-14 00:03:13 +0200

    Copying qwerty, raise, lower from Planck while adding a blank function layer

--- keyboards/keyboardio/atreus/keymaps/sigul/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/sigul/keymap.c
@@ -4,40 +4,85 @@
 // This is the canonical layout file for the Quantum project. If you want to add another keyboard,
 
 #include QMK_KEYBOARD_H
-
-enum layer_names {
-    _QW,
-    _RS,
-    _LW,
-};
+#include "muse.h"
+#include "keymap_italian_osx_ansi.h"
+#include "sigul.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QW] = LAYOUT( /* Qwerty */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT ),
+  [_QWERTY] = LAYOUT( /* Qwerty */ 
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * |   Q  |   W  |  E   |  R   |   T  |      |      |   Y  |   U  |   I  |   O  |   P  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |   A  |   S  |  D  |   F   |   G  |      |      |   H  |   J  |   K  |   L  |  ;:  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |Shft/Z|   X  |  C   |  V   | MS/B |  /?  |  '"  |   N  |   M  |  ,<  |  .>  |S/Ent |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |ESCFN | Ctrl | Alt  | GUI  |Lower |Sh/Tab| Space| Raise| Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+    IT_Q,		IT_W,		IT_E,		IT_R,  		IT_T,    				IT_Y,   IT_U,    IT_I,    IT_O,    IT_P,    
+    IT_A,		IT_S,		IT_D,		IT_F,  		IT_G,    				IT_H,  	IT_J,    IT_K,    IT_L,    IT_SCCL, 
+    MT(MOD_LSFT, IT_Z),	IT_X,		IT_C,		IT_V,  		MS_B,	IT_SLQS,		IT_APDQ,IT_N,   IT_M,    IT_CMLS, IT_DTMR, MT(MOD_RSFT, KC_ENT),
+    ESCFN,		KC_LCTL,	KC_LALT,	KC_LGUI,   	LOWER,	MT(MOD_LSFT, KC_TAB),	KC_SPC, RAISE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+),
 
-  /*
-   *  !       @     up     {    }        ||     pgup    7     8     9    *
-   *  #     left   down  right  $        ||     pgdn    4     5     6    +
-   *  [       ]      (     )    &        ||       `     1     2     3    \
-   * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
-   */
-  [_RS] = LAYOUT( /* [> RAISE <] */
-    KC_EXLM, KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                  KC_PGUP, KC_7,    KC_8,   KC_9, KC_BSPC,
-    KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                  KC_PGDN, KC_4,    KC_5,   KC_6, KC_BSLS,
-    KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, KC_PLUS,
-    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL ),
-  /*
-   * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
-   *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
-   *       volup             reset      ||             F1    F2    F3   F12
-   *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
-   */
-  [_LW] = LAYOUT( /* [> LOWER <] */
-    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS )
+[_RAISE] = LAYOUT( /* [> RAISE <] */
+
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |  1   |   2  |   3  |   4  |   5  |      |      |   6  |   7  |   8  |   9  |   0  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |  `   |      | SGCOM| DESK |      |      |      |   -  |   +  |   è  |   ì  |  ù   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Caps |      | PHONE| SVIV |VIVERE|      |      |      |      |  [   |   ]  |  \   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      | Home | PgDn | PgUp | End  |
+ * `-----------------------------------------------------------------------------------'
+ */
+    IT_1,     IT_2,    IT_3,	 IT_4,    IT_5,   		    IT_6,    IT_7,    IT_8,    IT_9,    IT_0,
+    IT_GRAVE, _______, SECRET2, SECRET1, _______, 		    IT_MINS, IT_PLUS, IT_EACC, IT_IACC, IT_UACC,
+    KC_CAPS,  _______, SECRET0, SECRET3, SECRET4, _______, _______, _______, _______, IT_LBRC, IT_RBRC, IT_BSLS,
+    _______,  _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+),
+
+  [_LOWER] = LAYOUT( /* [> LOWER <] */
+/* Lower
+ * ,-----------------------------------------------------------------------------------.
+ * |  !   |   @  |   #  |   $  |  %   |      |      |   &  |   *  |  (   |  )   |Bkspc |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |  ~   |  F1  |  F2  |  F3  |  F4  |      |      |   _  |   =  |  é   |  ò   |  à   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |  ^   |  F6  |  F7  |  F8  |  F9  |  F5  |      |   §  |   ±  |  {   |  }   |  |   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+
+    IT_EXLM,	IT_AT,   IT_SHRP, IT_DLR,  IT_PERC,			IT_AMPR,    IT_ASTR, IT_LPRN,    IT_RPRN, KC_BSPC,
+    IT_TILDE, 	KC_F1,   KC_F2,   KC_F3,   KC_F4,			S(IT_MINS), IT_EQL,  S(IT_EACC), IT_OACC, IT_AACC,
+    IT_CRC, 	KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F5,   _______,	S(IT_UACC), IT_PLMN, IT_LCBR,    IT_RCBR, IT_PIPE,
+    _______,  	_______, _______, _______, _______, _______, _______, 	_______,    KC_MNXT, KC_VOLD,    KC_VOLU, KC_MPLY
+),
+
+  [_FN] = LAYOUT( /* [> FUNTION <] */
+/* Lower
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      |      |      |      | Del  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      | left | down |  up  | right|      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+
+    _______,  	_______, _______, _______, _______,                     _______, _______, _______, _______, KC_DEL,
+    _______,  	_______, _______, _______, _______,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+    _______,  	_______, _______, _______, _______, _______, _______, 	_______, _______, _______, _______, _______,
+    _______,  	_______, _______, _______, _______, _______, _______, 	_______, _______, _______, _______, _______
+)
+
 };
