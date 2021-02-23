e40691b708fdad945f0082fe82f28723356d6890
sigul <desk@silviogulizia.com>
2021-02-23 16:47:33 +0100

    add italian keys

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -1,55 +1,56 @@
 // generated from users/manna-harbour_miryoku/miryoku.org  -*- buffer-read-only: t -*-
 
 #include "manna-harbour_miryoku.h"
-
+#include "keymap_italian_osx_ansi.h"
+ 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined MIRYOKU_ALPHAS_COLEMAK
   [BASE] = LAYOUT_miryoku(
-    KC_Q,              KC_W,              KC_F,              KC_P,              KC_G,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,
-    LGUI_T(KC_A),      LALT_T(KC_R),      LCTL_T(KC_S),      LSFT_T(KC_T),      KC_D,              KC_H,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),
-    KC_Z,              ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_K,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    KC_SLSH,
+    IT_Q,              IT_W,              IT_F,              IT_P,              IT_G,              IT_J,              IT_L,              IT_U,              IT_Y,              IT_QUOT,
+    LCTL_T(IT_A),      LALT_T(IT_R),      LGUI_T(IT_S),      LSFT_T(IT_T),      IT_D,              IT_H,              LSFT_T(IT_N),      LCTL_T(IT_E),      LALT_T(IT_I),      LGUI_T(IT_O),
+    IT_Z,              ALGR_T(IT_X),      IT_C,              IT_V,              IT_B,              IT_K,              IT_M,              IT_COMM,           ALGR_T(IT_DOT),    IT_SLSH,
     U_NP,              U_NP,              LT(MEDR, KC_ESC),  LT(NAVR, KC_SPC),  LT(MOUR, KC_TAB),  LT(NSSL, KC_ENT),  LT(NSL, KC_BSPC),  LT(FUNL, KC_DEL),  U_NP,              U_NP
   ),
 #elif defined MIRYOKU_ALPHAS_COLEMAKDHK
   [BASE] = LAYOUT_miryoku(
-    KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,
-    LGUI_T(KC_A),      LALT_T(KC_R),      LCTL_T(KC_S),      LSFT_T(KC_T),      KC_G,              KC_K,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),
-    KC_Z,              ALGR_T(KC_X),      KC_C,              KC_D,              KC_V,              KC_M,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    KC_SLSH,
+    IT_Q,              IT_W,              IT_F,              IT_P,              IT_B,              IT_J,              IT_L,              IT_U,              IT_Y,              IT_QUOT,
+    LGUI_T(IT_A),      LALT_T(IT_R),      LCTL_T(IT_S),      LSFT_T(IT_T),      IT_G,              IT_K,              LSFT_T(IT_N),      LCTL_T(IT_E),      LALT_T(IT_I),      LGUI_T(IT_O),
+    IT_Z,              ALGR_T(IT_X),      IT_C,              IT_D,              IT_V,              IT_M,              IT_H,              IT_COMM,           ALGR_T(IT_DOT),    IT_SLSH,
     U_NP,              U_NP,              LT(MEDR, KC_ESC),  LT(NAVR, KC_SPC),  LT(MOUR, KC_TAB),  LT(NSSL, KC_ENT),  LT(NSL, KC_BSPC),  LT(FUNL, KC_DEL),  U_NP,              U_NP
   ),
 #elif defined MIRYOKU_ALPHAS_DVORAK
   [BASE] = LAYOUT_miryoku(
-    KC_QUOT,           KC_COMM,           KC_DOT,            KC_P,              KC_Y,              KC_F,              KC_G,              KC_C,              KC_R,              KC_L,
-    LGUI_T(KC_A),      LALT_T(KC_O),      LCTL_T(KC_E),      LSFT_T(KC_U),      KC_I,              KC_D,              LSFT_T(KC_H),      LCTL_T(KC_T),      LALT_T(KC_N),      LGUI_T(KC_S),
-    KC_SLSH,           ALGR_T(KC_Q),      KC_J,              KC_K,              KC_X,              KC_B,              KC_M,              KC_W,              ALGR_T(KC_V),      KC_Z,
+    IT_QUOT,           IT_COMM,           IT_DOT,            IT_P,              IT_Y,              IT_F,              IT_G,              IT_C,              IT_R,              IT_L,
+    LGUI_T(IT_A),      LALT_T(IT_O),      LCTL_T(IT_E),      LSFT_T(IT_U),      IT_I,              IT_D,              LSFT_T(IT_H),      LCTL_T(IT_T),      LALT_T(IT_N),      LGUI_T(IT_S),
+    IT_SLSH,           ALGR_T(IT_Q),      IT_J,              IT_K,              IT_X,              IT_B,              IT_M,              IT_W,              ALGR_T(IT_V),      IT_Z,
     U_NP,              U_NP,              LT(MEDR, KC_ESC),  LT(NAVR, KC_SPC),  LT(MOUR, KC_TAB),  LT(NSSL, KC_ENT),  LT(NSL, KC_BSPC),  LT(FUNL, KC_DEL),  U_NP,              U_NP
   ),
 #elif defined MIRYOKU_ALPHAS_HALMAK
   [BASE] = LAYOUT_miryoku(
-    KC_W,              KC_L,              KC_R,              KC_B,              KC_Z,              KC_QUOT,           KC_Q,              KC_U,              KC_D,              KC_J,
-    LGUI_T(KC_S),      LALT_T(KC_H),      LCTL_T(KC_N),      LSFT_T(KC_T),      KC_COMM,           KC_DOT,            LSFT_T(KC_A),      LCTL_T(KC_E),      LALT_T(KC_O),      LGUI_T(KC_I),
-    KC_F,              ALGR_T(KC_M),      KC_V,              KC_C,              KC_SLSH,           KC_G,              KC_P,              KC_X,              ALGR_T(KC_K),      KC_Y,
+    IT_W,              IT_L,              IT_R,              IT_B,              IT_Z,              IT_QUOT,           IT_Q,              IT_U,              IT_D,              IT_J,
+    LGUI_T(IT_S),      LALT_T(IT_H),      LGUI_T(IT_N),      LSFT_T(IT_T),      IT_COMM,           IT_DOT,            LSFT_T(IT_A),      LCTL_T(IT_E),      LALT_T(IT_O),      LGUI_T(IT_I),
+    IT_F,              ALGR_T(IT_M),      IT_V,              IT_C,              IT_SLSH,           IT_G,              IT_P,              IT_X,              ALGR_T(IT_K),      IT_Y,
     U_NP,              U_NP,              LT(MEDR, KC_ESC),  LT(NAVR, KC_SPC),  LT(MOUR, KC_TAB),  LT(NSSL, KC_ENT),  LT(NSL, KC_BSPC),  LT(FUNL, KC_DEL),  U_NP,              U_NP
   ),
 #elif defined MIRYOKU_ALPHAS_WORKMAN
   [BASE] = LAYOUT_miryoku(
-    KC_Q,              KC_D,              KC_R,              KC_W,              KC_B,              KC_J,              KC_F,              KC_U,              KC_P,              KC_QUOT,
-    LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_H),      LSFT_T(KC_T),      KC_G,              KC_Y,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_O),      LGUI_T(KC_I),
-    KC_Z,              ALGR_T(KC_X),      KC_M,              KC_C,              KC_V,              KC_K,              KC_L,              KC_COMM,           ALGR_T(KC_DOT),    KC_SLSH,
+    IT_Q,              IT_D,              IT_R,              IT_W,              IT_B,              IT_J,              IT_F,              IT_U,              IT_P,              IT_QUOT,
+    LGUI_T(IT_A),      LALT_T(IT_S),      LCTL_T(IT_H),      LSFT_T(IT_T),      IT_G,              IT_Y,              LSFT_T(IT_N),      LCTL_T(IT_E),      LALT_T(IT_O),      LGUI_T(IT_I),
+    IT_Z,              ALGR_T(IT_X),      IT_M,              IT_C,              IT_V,              IT_K,              IT_L,              IT_COMM,           ALGR_T(IT_DOT),    IT_SLSH,
     U_NP,              U_NP,              LT(MEDR, KC_ESC),  LT(NAVR, KC_SPC),  LT(MOUR, KC_TAB),  LT(NSSL, KC_ENT),  LT(NSL, KC_BSPC),  LT(FUNL, KC_DEL),  U_NP,              U_NP
   ),
 #elif defined MIRYOKU_ALPHAS_QWERTY
   [BASE] = LAYOUT_miryoku(
-    KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,
-    LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),
-    KC_Z,              ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    KC_SLSH,
+    IT_Q,              IT_W,              IT_E,              IT_R,              IT_T,              IT_Y,              IT_U,              IT_I,              IT_O,              IT_P,
+    LCTL_T(IT_A),      LALT_T(IT_S),      LGUI_T(IT_D),      LSFT_T(IT_F),      IT_G,              IT_H,              LSFT_T(IT_J),      LGUI_T(IT_K),      LALT_T(IT_L),      LCTL_T(IT_QUOT),
+    IT_Z,              ALGR_T(IT_X),      IT_C,              IT_V,              IT_B,              IT_N,              IT_M,              IT_COMM,           ALGR_T(IT_DOT),    IT_SLSH,
     U_NP,              U_NP,              LT(MEDR, KC_ESC),  LT(NAVR, KC_SPC),  LT(MOUR, KC_TAB),  LT(NSSL, KC_ENT),  LT(NSL, KC_BSPC),  LT(FUNL, KC_DEL),  U_NP,              U_NP
   ),
 #else
   [BASE] = LAYOUT_miryoku(
-    KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,
-    LGUI_T(KC_A),      LALT_T(KC_R),      LCTL_T(KC_S),      LSFT_T(KC_T),      KC_G,              KC_M,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),
-    KC_Z,              ALGR_T(KC_X),      KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    KC_SLSH,
+    IT_Q,              IT_W,              IT_F,              IT_P,              IT_B,              IT_J,              IT_L,              IT_U,              IT_Y,              IT_QUOT,
+    LGUI_T(IT_A),      LALT_T(IT_R),      LCTL_T(IT_S),      LSFT_T(IT_T),      IT_G,              IT_M,              LSFT_T(IT_N),      LCTL_T(IT_E),      LALT_T(IT_I),      LGUI_T(IT_O),
+    IT_Z,              ALGR_T(IT_X),      IT_C,              IT_D,              IT_V,              IT_K,              IT_H,              IT_COMM,           ALGR_T(IT_DOT),    IT_SLSH,
     U_NP,              U_NP,              LT(MEDR, KC_ESC),  LT(NAVR, KC_SPC),  LT(MOUR, KC_TAB),  LT(NSSL, KC_ENT),  LT(NSL, KC_BSPC),  LT(FUNL, KC_DEL),  U_NP,              U_NP
   ),
 #endif
@@ -74,20 +75,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 #else
   [NAVR] = LAYOUT_miryoku(
-    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+    RESET,   LGUI_(IT_W),    U_NA,    U_NA,    U_NA,    U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
+    KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, U_NA,    KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
     U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,
     U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_ENT,  KC_BSPC, KC_DEL,  U_NP,    U_NP
   ),
   [MOUR] = LAYOUT_miryoku(
     RESET,   U_NA,    U_NA,    U_NA,    U_NA,    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    U_NU,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
+    KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, U_NA,    U_NU,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
     U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NU,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
     U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_BTN1, KC_BTN3, KC_BTN2, U_NP,    U_NP
   ),
   [MEDR] = LAYOUT_miryoku(
     RESET,   U_NA,    U_NA,    U_NA,    U_NA,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI,
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    U_NU,    KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
+    KC_LCTL, KC_LGUI, KC_LCTL, KC_LSFT, U_NA,    U_NU,    KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
     U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,
     U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_MSTP, KC_MPLY, KC_MUTE, U_NP,    U_NP
   ),
@@ -105,15 +106,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     U_NP,    U_NP,    KC_APP,  KC_SPC,  KC_TAB,  U_NA,    U_NA,    U_NA,    U_NP,    U_NP
   ),
   [NSL] = LAYOUT_miryoku(
-    KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
-    KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,  U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
-    U_NP,    U_NP,    KC_DOT,  KC_0,    KC_MINS, U_NA,    U_NA,    U_NA,    U_NP,    U_NP
+    IT_LBRC, IT_7,    IT_8,    IT_9,    IT_RBRC, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
+    IT_SCLN, IT_4,    IT_5,    IT_6,    IT_EQL,  U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+    IT_GRV,  IT_1,    IT_2,    IT_3,    IT_BSLS, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
+    U_NP,    U_NP,    IT_DOT,  IT_0,    IT_MINS, U_NA,    U_NA,    U_NA,    U_NP,    U_NP
   ),
   [NSSL] = LAYOUT_miryoku(
-    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
-    KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS, U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
-    U_NP,    U_NP,    KC_LPRN, KC_RPRN, KC_UNDS, U_NA,    U_NA,    U_NA,    U_NP,    U_NP
+    IT_LCBR, IT_AMPR, IT_ASTR, IT_LPRN, IT_RCBR, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
+    IT_COLN, IT_DLR,  IT_PERC, IT_CIRC, IT_PLUS, U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+    IT_TILD, IT_EXLM, IT_AT,   IT_HASH, IT_PIPE, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
+    U_NP,    U_NP,    IT_LPRN, IT_RPRN, IT_UNDS, U_NA,    U_NA,    U_NA,    U_NP,    U_NP
   )
 };
