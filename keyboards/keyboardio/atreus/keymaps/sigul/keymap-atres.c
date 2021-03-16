85342c37a62a100e2b679e4176f5cf3b8b023e53
sigul <desk@silviogulizia.com>
2021-03-16 15:32:35 +0100

    updates

---
+++ keyboards/keyboardio/atreus/keymaps/sigul/keymap-atres.c
@@ -0,0 +1,249 @@
+// Copyright (C) 2019, 2020  Keyboard.io, Inc
+//
+// this is the style you want to emulate.
+// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+
+#include QMK_KEYBOARD_H
+#include "muse.h"
+#include "keymap_italian_osx_ansi.h"
+#include "sigul.h"
+
+
+
+// Left-hand home row mods
+#define CTL_A LCTL_T(IT_A)
+#define ALT_S LALT_T(IT_S)
+#define CMD_D LGUI_T(IT_D)
+#define FN_G LT(FN, IT_G)
+#define SFT_F LSFT_T(IT_F)
+
+#define SP_NAV LT(NAVIGATION, KC_SPC)
+
+// Right-hand home row mods
+#define CTL_SC RCTL_T(IT_SCCL)
+#define ALT_L RALT_T(IT_L)
+#define CMD_K RGUI_T(IT_K)
+#define SFT_J RSFT_T(IT_J)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT( /* Qwerty */ 
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * |   Q  |   W  |  E   |  R   |   T  |      |      |   Y  |   U  |   I  |   O  |   P  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |   A  |   S  |  D  |   F   |   G  |      |      |   H  |   J  |   K  |   L  |  ;:  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |Shft/Z|   X  |  C   |  V   | MS/B |  /?  |  '"  |   N  |   M  |  ,<  |  .>  |S/Ent |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |ESCFN | Ctrl | Alt  | GUI  |Lower |Sh/Tab| Space| Raise| Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+    IT_Q,		IT_W,		IT_E,		IT_R,  			IT_T,    					          IT_Y,   IT_U,    IT_I,    IT_O,    IT_P,    
+    CTL_A,  		ALT_S,		CMD_D,		SFT_F, 		 FN_G,  						  IT_H,  SFT_J,    CMD_K,   ALT_L,   IT_APDQ, 
+    SFT_T(IT_Z),	IT_X,		IT_C,		IT_V,  			MS_B,			IT_SLQS,IT_SCCL,      	         IT_N,   IT_M,    IT_CMLS, IT_DTMR, IT_SLQS,
+    CTL_T(KC_ESC),	ALT_T(KC_TAB), 	KC_LGUI, 	SFT_T(KC_ESC),		LT(FN   ,KC_ENT), 	LT(LOWER,KC_TAB),       LT(LOWER,KC_SPC), LT(RAISE,KC_BSPC ), IT_SCCL, KC_DOWN, KC_UP,   KC_RGHT
+),
+
+[_RAISE] = LAYOUT( /* [> RAISE <] */
+
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |  1   |   2  |   3  |   4  |   5  |      |      |   6  |   7  |   8  |   9  |  0   |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |  `   |      | SGCOM| DESK |      |      |      |   -  |   +  |   è  |   ì  |  ù   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Caps |      | PHONE| SVIV |VIVERE|      |      |      |      |  [   |   ]  |  \   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      | Home | PgDn | PgUp | End  |
+ * `-----------------------------------------------------------------------------------'
+ */
+    IT_1,     IT_2,    IT_3,	 IT_4,    IT_5,   		    IT_6,    IT_7,    IT_8,    IT_9,    IT_0   ,
+    IT_GRAVE, SECRET2, SECRET1, _______, _______, 		    IT_MINS, IT_PLUS, IT_EACC, IT_IACC, IT_UACC,
+    KC_CAPS,  SECRET0, SECRET3, SECRET4, _______, _______, _______, _______, SECRET5, IT_LBRC, IT_RBRC, IT_BSLS,
+    _______,  _______, _______, _______, _______, _______, KC_BSPC, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+),
+
+  [_LOWER] = LAYOUT( /* [> LOWER <] */
+/* Lower
+ * ,-----------------------------------------------------------------------------------.
+ * |  !   |   @  |   #  |   $  |  %   |      |      |   &  |   *  |  (   |  )   | Bspc |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |  ~   |  F1  |  F2  |  F3  |  F4  |      |      |   _  |   =  |  é   |  ò   |  à   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |  ^   |  F6  |  F7  |  F8  |  F9  |  F5  |      |   §  |   ±  |  {   |  }   |  |   |
+
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+
+    IT_EXLM,	IT_AT,   IT_SHRP, IT_DLR,  IT_PERC,			IT_AMPR,    IT_ASTR, IT_LPRN,    IT_RPRN, KC_BSPC,
+    IT_TILDE, 	KC_F1,   KC_F2,   KC_F3,   KC_F4,			S(IT_MINS), IT_EQL,  S(IT_EACC), IT_OACC, IT_AACC,
+    IT_CRC, 	KC_F5,   KC_F5,   KC_F7,   KC_F8,  _______,  _______,	S(IT_UACC), IT_PLMN, IT_LCBR,    IT_RCBR, IT_PIPE,
+    _______,  	_______, _______, _______, KC_TAB, _______, _______, 	_______,    KC_MNXT, KC_VOLD,    KC_VOLU, KC_MPLY
+),
+
+  [_FN] = LAYOUT( /* [> FUNTION <] */
+/* Function
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |      |      |      |      |      |      |      |      |      |      | Del  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      | left | down |  up  | right|      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      | Del  |      |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+
+    KC_TAB ,  	_______, _______, RESET  , _______,                     _______, _______, _______, _______, KC_BSPC,
+    _______,  	_______, _______, _______, _______,                     _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+    _______,  	_______, _______, _______, _______, _______, _______, 	_______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
+    _______,  	_______, _______, _______, KC_DEL, _______, IT_SCCL, 	KC_DEL , KC_BTN1, KC_BTN3, KC_BTN2,_______
+),
+
+ [_NAV] = LAYOUT( /* [> NAVIGATION <] */
+/* Navigation
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |      |      |      |      |      |      |      |      |      |      | Del  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Ctrl | Alt  | Shift| GUI  |      |      |      | left | down |  up  | right|      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      | Del  |      |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+
+    KC_TAB ,  	_______, _______, _______, _______,                     _______, _______, _______, _______, KC_BSPC,
+    CTL_A, 	  ALT_S,   CMD_D,  SFT_F, FN_G,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+    G(IT_Z),  	G(IT_X), G(IT_V), G(IT_C), _______,_______, _______, 	_______, _______, _______, _______, _______,
+    _______,  	_______, _______, _______, KC_DEL, _______, _______, 	_______, _______, _______, _______, _______
+),
+
+  [_NUMPAD] = LAYOUT( /* [> NUMPAD <] */
+/* Numpad
+ * ,-----------------------------------------------------------------------------------.
+ * |  1   |  2   |  3   |   4  |   5  |      |      |   6  |   7  |  8   |  9   |  0   |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      | 	   |   4  |  5   |  6   |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |   1  |  2   |  3   |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+
+    IT_1,     IT_2,    IT_3,	 IT_4,    IT_5,   		        IT_6,    IT_7,    IT_8,    IT_9,    IT_0,
+    _______,  	_______, _______, _______, _______,                     _______, IT_4,    IT_5,    IT_6, _______,
+    _______,  	_______, _______, _______, _______, _______, _______, 	_______, IT_1,    IT_2,    IT_3, _______,
+    _______,  	_______, _______, _______, _______, _______, _______, 	_______, _______, _______, _______, _______
+),
+
+  [_ADJUST] = LAYOUT( /* [> ADJUST <] */
+
+/* Adjust (Lower + Raise)
+ * ,-----------------------------------------------------------------------------------.
+ * |      |Querty|      |ResetE|Reset |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |Debug |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |  @   |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+    DF(QWERTY), _______, EEP_RST, RESET,   _______,                    _______, _______, _______, _______, _______,
+    _______, _______,    _______, DEBUG,   _______,                    _______, _______, _______, _______, _______,
+    _______, _______,    _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
+    _______, _______,    _______, _______, _______, IT_AT, _______, _______, _______,  _______, _______, _______
+),
+
+};
+
+bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+ case CTL_A:
+return true;
+case ALT_S:
+return true;
+case CMD_D:
+return true;
+case SFT_F:
+return true;
+case FN_G:
+return true;
+case SP_NAV :
+return true;
+case CTL_SC:
+return true;
+case ALT_L:
+return true;
+case CMD_K :
+return true;
+case SFT_J :
+return true;
+case SFT_T(IT_Z):
+return true;
+       default:
+            return false;
+    }
+}
+
+bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+case CTL_A:
+return true;
+case ALT_S:
+return true;
+case CMD_D:
+return true;
+case FN_G:
+return true;
+case SFT_F:
+return true;
+case SP_NAV :
+return true;
+case CTL_SC:
+return true;
+case ALT_L:
+return true;
+case CMD_K :
+return true;
+case SFT_J :
+return true;
+case SFT_T(IT_Z):
+return true;
+       default:
+            return false;
+    }
+}
+
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+case CTL_A:
+return true;
+case ALT_S:
+return true;
+case CMD_D:
+return true;
+case FN_G:
+return true;
+case SFT_F:
+return true;
+case SP_NAV :
+return true;
+case CTL_SC:
+return true;
+case ALT_L:
+return true;
+case CMD_K :
+return true;
+case SFT_J :
+return true;       
+case SFT_T(IT_Z):
+return true;
+       default:
+            return false;
+    }
+}
--- keyboards/keyboardio/atreus/keymaps/sigul/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/sigul/keymap.c
@@ -9,241 +9,70 @@
 #include "sigul.h"
 
 
-
-// Left-hand home row mods
-#define CTL_A LCTL_T(IT_A)
 #define ALT_S LALT_T(IT_S)
 #define CMD_D LGUI_T(IT_D)
-#define FN_G LT(FN, IT_G)
 #define SFT_F LSFT_T(IT_F)
-
-#define SP_NAV LT(NAVIGATION, KC_SPC)
-
-// Right-hand home row mods
-#define CTL_SC RCTL_T(IT_SCCL)
-#define ALT_L RALT_T(IT_L)
-#define CMD_K RGUI_T(IT_K)
-#define SFT_J RSFT_T(IT_J)
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT( /* Qwerty */ 
-/* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * |   Q  |   W  |  E   |  R   |   T  |      |      |   Y  |   U  |   I  |   O  |   P  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   A  |   S  |  D  |   F   |   G  |      |      |   H  |   J  |   K  |   L  |  ;:  |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |Shft/Z|   X  |  C   |  V   | MS/B |  /?  |  '"  |   N  |   M  |  ,<  |  .>  |S/Ent |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |ESCFN | Ctrl | Alt  | GUI  |Lower |Sh/Tab| Space| Raise| Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
- */
-    IT_Q,		IT_W,		IT_E,		IT_R,  			IT_T,    					          IT_Y,   IT_U,    IT_I,    IT_O,    IT_P,    
-    CTL_A,  		ALT_S,		CMD_D,		SFT_F, 		 FN_G,  						  IT_H,  SFT_J,    CMD_K,   ALT_L,   IT_APDQ, 
-    SFT_T(IT_Z),	IT_X,		IT_C,		IT_V,  			MS_B,			IT_SLQS,IT_SCCL,      	         IT_N,   IT_M,    IT_CMLS, IT_DTMR, IT_SLQS,
-    CTL_T(KC_ESC),	ALT_T(KC_TAB), 	KC_LGUI, 	SFT_T(KC_ESC),		LT(FN   ,KC_ENT), 	LT(LOWER,KC_TAB),       LT(LOWER,KC_SPC), LT(RAISE,KC_BSPC ), IT_SCCL, KC_DOWN, KC_UP,   KC_RGHT
-),
-
-[_RAISE] = LAYOUT( /* [> RAISE <] */
+#define ALT_L LALT_T(IT_L)
+#define CMD_K LGUI_T(IT_K)
+#define SFT_J LSFT_T(IT_J)
+#define ALG_X ALGR_T(IT_X)
+#define MED_ESC LT(MEDR, KC_ESC)
+#define NAV_ENT LT(NAVR, KC_ENT)
+#define MOU_TAB LT(MOUR, KC_TAB)
+#define SYM_SPC LT(NSSL, KC_SPC)
+#define NUM_BSP LT(NSL, KC_BSPC)
+#define UNDO LCMD(IT_Z)
+#define REDO LCMD(S(IT_Z))
+#define CUT LCMD(IT_X)
+#define COPY LCMD(IT_C)
+#define PASTE LCMD(IT_V)
 
 
-/* Raise
- * ,-----------------------------------------------------------------------------------.
- * |  1   |   2  |   3  |   4  |   5  |      |      |   6  |   7  |   8  |   9  |  0   |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |  `   |      | SGCOM| DESK |      |      |      |   -  |   +  |   è  |   ì  |  ù   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Caps |      | PHONE| SVIV |VIVERE|      |      |      |      |  [   |   ]  |  \   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      | Home | PgDn | PgUp | End  |
- * `-----------------------------------------------------------------------------------'
- */
-    IT_1,     IT_2,    IT_3,	 IT_4,    IT_5,   		    IT_6,    IT_7,    IT_8,    IT_9,    IT_0   ,
-    IT_GRAVE, SECRET2, SECRET1, _______, _______, 		    IT_MINS, IT_PLUS, IT_EACC, IT_IACC, IT_UACC,
-    KC_CAPS,  SECRET0, SECRET3, SECRET4, _______, _______, _______, _______, SECRET5, IT_LBRC, IT_RBRC, IT_BSLS,
-    _______,  _______, _______, _______, _______, _______, KC_BSPC, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
-),
+[BASE] = LAYOUT_split_3x6_3(
+ 	IT_Q,		IT_W,		IT_E,		IT_R,		IT_T,								IT_Y,		IT_U,		IT_I,		IT_O,		IT_P,
+ 	CTL_A,		ALT_S,		CMD_D,		SFT_F,		IT_G,								IT_H,		SFT_J,		CMD_K,		ALT_L,		IT_APDQ,
+ 	IT_Z,		ALG_X,		IT_C,		IT_V,		IT_B,		XXXXXXX,	XXXXXXX,	IT_N,		IT_M,		IT_CMLS,	IT_DTMR,	KC_ENT,	
+ 	XXXXXXX,	XXXXXXX,	XXXXXXX,	MED_ESC,	NAV_ENT,	MOU_TAB,	SYM_SPC,	NUM_BSP,	IT_SLQS,	XXXXXXX,	XXXXXXX,	XXXXXXX
+  ),
 
-  [_LOWER] = LAYOUT( /* [> LOWER <] */
-/* Lower
- * ,-----------------------------------------------------------------------------------.
- * |  !   |   @  |   #  |   $  |  %   |      |      |   &  |   *  |  (   |  )   | Bspc |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |  ~   |  F1  |  F2  |  F3  |  F4  |      |      |   _  |   =  |  é   |  ò   |  à   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  ^   |  F6  |  F7  |  F8  |  F9  |  F5  |      |   §  |   ±  |  {   |  }   |  |   |
+[NAVR] = LAYOUT_split_3x6_3(
+	_______,	_______,	_______,	_______,	_______,							REDO,		PASTE,		COPY,		CUT,		UNDO,
+	_______,	_______,	_______,	_______,	_______,							KC_CAPS,	KC_LEFT,	KC_DOWN,	KC_UP,		KC_RGHT,
+	UNDO,		CUT,		COPY,		PASTE,		REDO,		_______,	_______,	_______,	KC_HOME,	KC_PGDN,	KC_PGUP,	KC_END,
+	_______,	_______,	_______,	_______,	_______,	_______,	IT_COLN,	KC_DEL,		IT_SCCL,	_______,	_______,	_______	
+ ),
 
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
+[MOUR] = LAYOUT_split_3x6_3(
+    _______,	_______,	_______,	_______,	_______,							S(IT_EACC), IT_UACC,	IT_IACC,	IT_OACC,	IT_AACC,
+	_______,	_______,	_______,	_______,	_______,							IT_EACC, 	KC_MS_L, 	KC_MS_D, 	KC_MS_U, 	KC_MS_R,
+	UNDO,		CUT,		COPY,		PASTE,		REDO,		_______,	_______,	_______,	KC_WH_L, 	KC_WH_D, 	KC_WH_U, 	KC_WH_R,
+	_______,	_______,	_______,	_______,	_______,	_______,	KC_BTN1, 	KC_BTN2,	_______,	_______,	_______,	_______
+  ),
 
-    IT_EXLM,	IT_AT,   IT_SHRP, IT_DLR,  IT_PERC,			IT_AMPR,    IT_ASTR, IT_LPRN,    IT_RPRN, KC_BSPC,
-    IT_TILDE, 	KC_F1,   KC_F2,   KC_F3,   KC_F4,			S(IT_MINS), IT_EQL,  S(IT_EACC), IT_OACC, IT_AACC,
-    IT_CRC, 	KC_F5,   KC_F5,   KC_F7,   KC_F8,  _______,  _______,	S(IT_UACC), IT_PLMN, IT_LCBR,    IT_RCBR, IT_PIPE,
-    _______,  	_______, _______, _______, KC_TAB, _______, _______, 	_______,    KC_MNXT, KC_VOLD,    KC_VOLU, KC_MPLY
-),
+[MEDR] = LAYOUT_split_3x6_3(
+    _______,	_______,	_______,	_______,	_______,							RGB_TOG, 	RGB_MOD, 	RGB_HUI, 	RGB_SAI,	RGB_VAI,
+	_______,	_______,	_______,	_______,	_______,							_______, 	KC_MPRV, 	KC_VOLD, 	KC_VOLU, 	KC_MNXT,
+	UNDO,		CUT,		COPY,		PASTE,		REDO,		_______,	_______,	_______,	KC_WH_L, 	KC_WH_D, 	KC_WH_U, 	KC_WH_R,
+	_______,	_______,	_______,	_______,	_______,	_______,	KC_MSTP, 	KC_MPLY, 	KC_MUTE,	_______,	_______,	_______											
+  ),
 
-  [_FN] = LAYOUT( /* [> FUNTION <] */
-/* Function
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |      |      |      |      |      |      |      |      |      |      | Del  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      | left | down |  up  | right|      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      | Del  |      |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
+[NSL] = LAYOUT_split_3x6_3(
+    KC_1,   	KC_2,   	KC_3, 		KC_4,   	KC_5,								KC_6,		KC_7,   	KC_8,   	KC_9,  		KC_0,
+	IT_PLUS,	IT_MINS,	IT_ASTR,	IT_SLSH,	IT_EQL,								_______,	_______,	IT_LCBR,	IT_RBRC,	IT_SCCL,
+	_______,	_______,	_______,	IT_BSLS,	IT_GRV,		_______,	_______,	_______,	_______,	IT_LBRC,	IT_RBRC,	_______,
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______
+  ),
 
-    KC_TAB ,  	_______, _______, RESET  , _______,                     _______, _______, _______, _______, KC_BSPC,
-    _______,  	_______, _______, _______, _______,                     _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-    _______,  	_______, _______, _______, _______, _______, _______, 	_______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
-    _______,  	_______, _______, _______, KC_DEL, _______, IT_SCCL, 	KC_DEL , KC_BTN1, KC_BTN3, KC_BTN2,_______
-),
+[NSSL] = LAYOUT_split_3x6_3(
+   	IT_EXLM,	IT_AT,   	IT_HASH, 	IT_DLR,		IT_PERC, 							IT_CIRC,	IT_ASTR,	IT_LPRN, 	IT_RPRN,	IT_AMPR,
+	IT_TILD,	IT_UNDS, 	IT_PIPE,	_______,	_______,							_______,	_______,	IT_LCBR,	IT_LCBR,	_______,
+	_______,	_______,	_______,	IT_BSLS,	IT_GRV,		_______,	_______,	_______,	_______,	IT_LBRC,	IT_RBRC,	_______,
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______
+  ),
 
- [_NAV] = LAYOUT( /* [> NAVIGATION <] */
-/* Navigation
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |      |      |      |      |      |      |      |      |      |      | Del  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl | Alt  | Shift| GUI  |      |      |      | left | down |  up  | right|      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      | Del  |      |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
-
-    KC_TAB ,  	_______, _______, _______, _______,                     _______, _______, _______, _______, KC_BSPC,
-    CTL_A, 	  ALT_S,   CMD_D,  SFT_F, FN_G,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-    G(IT_Z),  	G(IT_X), G(IT_V), G(IT_C), _______,_______, _______, 	_______, _______, _______, _______, _______,
-    _______,  	_______, _______, _______, KC_DEL, _______, _______, 	_______, _______, _______, _______, _______
-),
-
-  [_NUMPAD] = LAYOUT( /* [> NUMPAD <] */
-/* Numpad
- * ,-----------------------------------------------------------------------------------.
- * |  1   |  2   |  3   |   4  |   5  |      |      |   6  |   7  |  8   |  9   |  0   |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      | 	   |   4  |  5   |  6   |      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |   1  |  2   |  3   |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-
-    IT_1,     IT_2,    IT_3,	 IT_4,    IT_5,   		        IT_6,    IT_7,    IT_8,    IT_9,    IT_0,
-    _______,  	_______, _______, _______, _______,                     _______, IT_4,    IT_5,    IT_6, _______,
-    _______,  	_______, _______, _______, _______, _______, _______, 	_______, IT_1,    IT_2,    IT_3, _______,
-    _______,  	_______, _______, _______, _______, _______, _______, 	_______, _______, _______, _______, _______
-),
-
-  [_ADJUST] = LAYOUT( /* [> ADJUST <] */
-
-/* Adjust (Lower + Raise)
- * ,-----------------------------------------------------------------------------------.
- * |      |Querty|      |ResetE|Reset |      |      |      |      |      |      |      |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Debug |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |  @   |      |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
-    DF(QWERTY), _______, EEP_RST, RESET,   _______,                    _______, _______, _______, _______, _______,
-    _______, _______,    _______, DEBUG,   _______,                    _______, _______, _______, _______, _______,
-    _______, _______,    _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
-    _______, _______,    _______, _______, _______, IT_AT, _______, _______, _______,  _______, _______, _______
-),
-
-};
-
-bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
- case CTL_A:
-return true;
-case ALT_S:
-return true;
-case CMD_D:
-return true;
-case SFT_F:
-return true;
-case FN_G:
-return true;
-case SP_NAV :
-return true;
-case CTL_SC:
-return true;
-case ALT_L:
-return true;
-case CMD_K :
-return true;
-case SFT_J :
-return true;
-case SFT_T(IT_Z):
-return true;
-       default:
-            return false;
-    }
-}
-
-bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-case CTL_A:
-return true;
-case ALT_S:
-return true;
-case CMD_D:
-return true;
-case FN_G:
-return true;
-case SFT_F:
-return true;
-case SP_NAV :
-return true;
-case CTL_SC:
-return true;
-case ALT_L:
-return true;
-case CMD_K :
-return true;
-case SFT_J :
-return true;
-case SFT_T(IT_Z):
-return true;
-       default:
-            return false;
-    }
-}
-
-bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-case CTL_A:
-return true;
-case ALT_S:
-return true;
-case CMD_D:
-return true;
-case FN_G:
-return true;
-case SFT_F:
-return true;
-case SP_NAV :
-return true;
-case CTL_SC:
-return true;
-case ALT_L:
-return true;
-case CMD_K :
-return true;
-case SFT_J :
-return true;       
-case SFT_T(IT_Z):
-return true;
-       default:
-            return false;
-    }
-}
+[FUNL] = LAYOUT_split_3x6_3(
+    KC_F1,   	KC_F2,   	KC_F3, 		KC_F4,   	KC_F5,								KC_F6,		KC_F7,   	KC_F8,   	KC_F9,  	KC_F10,
+	_______,	_______,	_______,	_______,	_______,							_______,	_______,	_______,	_______,	_______,
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,
+	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______,	_______			
+  )
